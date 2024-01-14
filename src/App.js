c2e8b8aadd8fa920bc85e56572019ed6ca16419d
ArtsemStankevich <artsemstankevich@gmail.com>
2024-01-14 14:23:40 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import Home from './componennts/Home';
 import About from './componennts/About';
 import ShowDataPage from './componennts/ShowDataPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/show-addresses" element={<ShowDataPage />} />
       </Routes>
